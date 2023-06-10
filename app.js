a779fb651e84ac6065a8a188db2e5b53f03f379a
shivam02544 <100270858+shivam02544@users.noreply.github.com>
2023-06-10 23:14:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const subs = document.querySelectorAll(".btn"),
+    socialMedia = document.querySelectorAll(".logo"),
+    like = document.querySelector(".bx-heart");
+
+subs[0].addEventListener('click', () => {
+    subs[0].innerText = 'Subscribed';
+})
+
+subs[1].addEventListener('click', () => {
+    window.open("https://www.instagram.com/direct/t/112527886963063/")
+})
+
+socialMedia[0].addEventListener('click', () => {
+    window.open("https://www.facebook.com/profile.php?id=100087205885778");
+})
+
+socialMedia[2].addEventListener('click', () => {
+    window.open("https://www.instagram.com/is__rajput/");
+})
+
+like.addEventListener('click', () => {
+    if (like.classList.contains("bx-heart")) {
+        console.log('helo')
+        like.classList.replace("bx-heart", "bxs-heart")
+        like.style.color = 'red'
+    } else {
+        like.classList.replace("bxs-heart", "bx-heart")
+        like.style.color = 'rgba(0, 0, 0, .5)'
+    }
+})
